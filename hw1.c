d5fbe0b61dae0c956fdd3f7a179cdad84231dd65
rkdwlsdb <kangyj982@gmail.com>
2022-09-14 21:53:47 +0900

    2주차 개념 확인 과제

--- hw1.c
+++ hw1.c
@@ -9,6 +9,6 @@ int main(void)
     printf("%d & %d = %d \n", num1, num2, num1 & num2);
     printf("%d | %d = %d \n", num1, num2, num1 | num2);
     printf("%d ^ %d = %d \n", num1, num2, num1 ^ num2);
-    
+       
     return 0;
 }
