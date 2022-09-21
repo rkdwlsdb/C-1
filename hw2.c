47d47bb949afd35165e72293c950897dc3db68aa
yujin kang <yujinkang@yujin.local>
2022-09-21 17:08:34 +0900

    3주차 과제

---
+++ hw2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    // 1 mile = 1.609 km
+
+    double km;
+    double mile;
+    printf("Please enter kilometers: ");
+    scanf("%lf", &km);
+
+    mile = km / 1.609;
+    printf("%.1lf km is equl to %.1lf miles.\n", km, mile);
+    
+    return 0;
+}
